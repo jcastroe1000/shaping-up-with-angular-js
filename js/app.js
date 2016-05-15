b94e71d4ee0ef6ae73d3457dadb0920e5f6256df
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-05-14 22:12:54 -0500

    adding a example with modules,controller, directives and jsons

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+	var app =angular.module('store',[]);
+	app.controller('StoreController',function(){
+		this.products=gems;
+	});
+	var gems =[
+
+	{
+		name:'Dodecahedron',
+		price: 2.95,
+		description:'algo',
+		soldOut:true,
+	},
+	{
+		name:'Pentagonal Gem',
+		price: 5.95,
+		description:'algo  2',
+		soldOut:false,
+	}
+	];
